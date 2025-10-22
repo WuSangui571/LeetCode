5587979f7e17775179a75b67f5177e6888ad06b8
sangui <shouhaoze@stu.sjzu.edu.cn>
2025-10-22 18:36:44 +0800

    2025-10-22-18:36

---
+++ 2025-10-22/src/main/java/Q_39.java
@@ -0,0 +1,59 @@
+import java.sql.PreparedStatement;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Author: sangui
+ * @CreateTime: 2025-10-22
+ * @Description: 39. 组合总和 去重复！！！！！！！！！
+ * @Version: 1.0
+ */
+public class Q_39 {
+    static List<List<Integer>> res = new ArrayList<>(new ArrayList<>());
+    public static void main(String[] args) {
+        int[] candidates = {2,3,6,7};
+        int target = 7;
+        // [[2,2,3],[7]]
+        combinationSum(candidates,target);
+
+        System.out.println(res);
+        //System.out.println("最终：" + res);
+        //System.out.println(res.size());
+//        for (int i = 0; i < res.size(); i++) {
+//            System.out.print("List " + i + ": ");
+//            for (Integer num : res.get(i)) {
+//                System.out.print(num + " ");
+//            }
+//            System.out.println();
+//        }
+    }
+    public static List<List<Integer>> combinationSum(int[] candidates, int target) {
+        f(candidates,new ArrayList<>(),target);
+        List<List<Integer>> res2 = res;
+        res = new ArrayList<>(new ArrayList<>());
+        return res2;
+    }
+    //
+    public static void f(int[] candidates,List<Integer> currentCandidates,int target){
+        int currentTarget = 0;
+        for (int i = 0; i < currentCandidates.size(); i++) {
+            currentTarget += currentCandidates.get(i);
+        }
+        //System.out.println(currentTarget);
+        if (currentTarget == target){
+            res.add(new ArrayList<>(currentCandidates));
+            //System.out.println("添加：" + currentCandidates);
+            return;
+        }
+        if (currentTarget > target){
+            //currentCandidates.removeLast();
+            return;
+        }
+        for (int i = 0; i < candidates.length; i++) {
+            currentCandidates.add(candidates[i]);
+            //System.out.println(currentCandidates);
+            f(candidates,currentCandidates,target);
+            currentCandidates.removeLast();
+        }
+    }
+}
