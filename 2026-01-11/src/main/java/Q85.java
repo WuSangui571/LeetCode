01aa4a96c4338df0c56ae5adf96ab44515e27c5f
sangui <shouhaoze@stu.sjzu.edu.cn>
2026-01-13 15:38:45 +0800

    feat:完成Q3453

--- 2026-01-11/src/main/java/Q85.java
+++ 2026-01-11/src/main/java/Q85.java
@@ -31,6 +31,46 @@ public class Q85 {
         System.out.println(res);
     }
 
+    public int maximalRectangle2(char[][] matrix) {
+        int[] height = new int[matrix[0].length];
+        int maxArea = 0;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] == '1') {
+                    height[j] += 1;
+                } else {
+                    height[j] = 0;
+                }
+            }
+            System.out.println("第「" + i + "」行，height = " + Arrays.toString(height));
+            ArrayDeque<Integer> stack = new ArrayDeque<>();
+            for (int j = 0; j < height.length; j++) {
+                if (stack.isEmpty() || height[j] > height[stack.peek()]) {
+                    stack.push(j);
+                    System.out.println("height 数组里值为的 " + height[j] + " 的坐标:「" + j + "」入栈了！此时stack=" + stack);
+                } else {
+                    while (!stack.isEmpty() && height[stack.peek()] >= height[j]) {
+                        int popIndex = stack.pop();
+                        int left = -1;
+                        if (!stack.isEmpty()) {
+                            left = stack.peek();
+                        }
+                        int width = j - left - 1;
+                        int area = width * height[popIndex];
+                        if (area > maxArea) {
+                            maxArea = area;
+                        }
+                        System.out.println("计算面积=" + width + "(宽)*" + height[popIndex] + "(长)=" + area);
+                        System.out.println("height[]中的 " + height[popIndex] + " 的坐标:「" + popIndex + "」出栈了！此时stack=" + stack);
+                    }
+                    stack.push(j);
+                    System.out.println("height[]中的 " + height[j] + " 的坐标:「" + j + "」入栈了！此时stack=" + stack);
+                }
+            }
+        }
+        return maxArea;
+    }
+
     public int maximalRectangle(char[][] matrix) {
         int[] height = new int[matrix[0].length];
         int maxArea = 0;
@@ -42,13 +82,13 @@ public class Q85 {
                     height[j] = 0;
                 }
             }
-//            System.out.println("第「" + i + "」行，height = " + Arrays.toString(height));
+            System.out.println("第「" + i + "」行，height = " + Arrays.toString(height));
             ArrayDeque<Integer> stack = new ArrayDeque<>();
             for (int j = 0; j <= height.length; j++) {
                 int cur = (j == height.length ? 0 : height[j]);
                 if (stack.isEmpty() || cur > height[stack.peek()]) {
                     stack.push(j);
-//                    System.out.println("height 数组里值为的 " + cur + " 的坐标:「" + j + "」入栈了！此时stack=" + stack);
+                    System.out.println("height 数组里值为的 " + cur + " 的坐标:「" + j + "」入栈了！此时stack=" + stack);
                 } else {
                     while (!stack.isEmpty() && height[stack.peek()] >= cur) {
                         int popIndex = stack.pop();
@@ -61,14 +101,16 @@ public class Q85 {
                         if (area > maxArea) {
                             maxArea = area;
                         }
-//                        System.out.println("计算面积=" + width + "(宽)*" + height[popIndex] + "(长)=" + area);
-//                        System.out.println("height[]中的 " + height[popIndex] + " 的坐标:「" + popIndex + "」出栈了！此时stack=" + stack);
+                        System.out.println("计算面积=" + width + "(宽)*" + height[popIndex] + "(长)=" + area);
+                        System.out.println("height[]中的 " + height[popIndex] + " 的坐标:「" + popIndex + "」出栈了！此时stack=" + stack);
                     }
                     stack.push(j);
-//                    System.out.println("height[]中的 " + cur + " 的坐标:「" + j + "」入栈了！此时stack=" + stack);
+                    System.out.println("height[]中的 " + cur + " 的坐标:「" + j + "」入栈了！此时stack=" + stack);
                 }
             }
         }
         return maxArea;
+
     }
+
 }
---
+++ 2026-01-13/src/main/java/Q3453.java
@@ -0,0 +1,58 @@
+import java.util.*;
+
+/**
+ * @Author: sangui
+ * @CreateTime: 2026-01-13
+ * @Description:
+ * @Version: 1.0
+ */
+public class Q3453 {
+    public static void main(String[] args) {
+        // 1.00000
+//        int[][] squares = {{0, 0, 1}, {2, 2, 1}};
+//        int[][] squares = {{2, 2, 1}, {0, 0, 1},};
+
+        // 1.16667
+        int[][] squares = {{0,0,2},{1,1,1}};
+
+
+        Q3453 obj = new Q3453();
+        double res = obj.separateSquares(squares);
+        System.out.println(res);
+    }
+
+    public double separateSquares(int[][] squares) {
+        double totalArea = 0L;
+        int n = squares.length;
+//        Hashtable<Integer, Integer> hashtable = new Hashtable<>();
+        List<int[]> list = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            // add total area
+            totalArea += ((long)squares[i][2] * squares[i][2]);
+            list.add(new int[]{squares[i][1],squares[i][2]});
+            list.add(new int[]{squares[i][1] + squares[i][2],(-1) * squares[i][2]});
+        }
+        list.sort(Comparator.comparingInt(a -> a[0]));
+        System.out.println("totalArea = " + totalArea);
+        System.out.println("list = ");
+        double currentArea = 0L;
+        double currentWidth = 0L;
+        for (int i = 0; i < list.size() - 1; i++) {
+            int y = list.get(i)[0];
+            int l_type = list.get(i)[1];
+            System.out.println(Arrays.toString(list.get(i)));
+
+            currentWidth += l_type;
+            double increase = currentWidth * (list.get(i+1)[0] - y);
+            if ((increase + currentArea) >= (totalArea/2)){
+                System.out.println("found in " + y + "~" + list.get(i+1)[0]);
+                double missingArea = totalArea/2 - currentArea;
+                double missingY = missingArea/currentWidth;
+                return y + missingY;
+            }
+            currentArea += increase;
+        }
+        return 0L;
+    }
+}
